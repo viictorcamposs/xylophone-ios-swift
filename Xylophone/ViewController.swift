8833e1122ad03ad6e642b3e3add427a33802dfdc
Victor Campos <campos.viictor@gmail.com>
2023-09-12 12:48:16 -0300

    dynamically play sound based on the button title

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,24 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(note: sender.currentTitle!)
+    }
     
-
+    func playSound(note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        
+        player = try! AVAudioPlayer(contentsOf: url!)
+        
+        player.play()
+                
+    }
 }
-
